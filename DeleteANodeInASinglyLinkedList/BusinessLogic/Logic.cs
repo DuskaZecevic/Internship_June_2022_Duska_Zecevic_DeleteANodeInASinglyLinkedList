fd4d9a488fa6b9635efc3a03ee95eeff9b6e0068
Duska <Duska@DESKTOP-O3AR66J>
2022-07-06 18:12:39 +0200

    Add project files.

---
+++ DeleteANodeInASinglyLinkedList/BusinessLogic/Logic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DeleteANodeInASinglyLinkedList.BusinessLogic
+{
+    public class Logic
+    {
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        Node head;
+        public void push(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.next = head;
+            head = new_node;
+        }
+        public void deleteNode(int key)
+        {
+            Node node = head, prev = null;
+
+            if (node != null && node.data == key)
+            {
+                head = node.next;
+                return;
+            }
+
+            while (node != null && node.data != key)
+            {
+                prev = node;
+                node = node.next;
+            }
+
+            if (node == null)
+                return;
+
+            prev.next = node.next;
+        }
+        public void printList()
+        {
+            Node node = head;
+            while (node != null)
+            {
+                Console.Write(node.data + " ");
+                node = node.next;
+            }
+            Console.WriteLine("\n");
+        }
+
+    }
+}
---
+++ DeleteANodeInASinglyLinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DeleteANodeInASinglyLinkedList.BusinessLogic;
+
+namespace DeleteANodeInASinglyLinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Logic list = new Logic();
+            list.Push(9);
+            list.Push(1);
+            list.Push(5);
+            list.Push(4);
+
+            Console.WriteLine("Linked list before deletion: ");
+            list.printList();
+
+            list.deleteNode(1);
+
+            Console.WriteLine("Linked list after deletion: ");
+            list.printList();
+        }
+       
+    }
+
+}
